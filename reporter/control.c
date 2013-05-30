b203be4df37f4517bd8a969c75b0e268f65e158a
senda <senda@52af7025-0ca6-e441-c252-aa76c1ea71f0>
2013-05-30 05:35:52 +0000

    pg_statsinfoの停止／再起動の結合試験のバグ改修  - ST-001: エージェント停止／起動コマンドでエラーが発生した時のログの表示形式が正しくない

--- reporter/control.c
+++ reporter/control.c
@@ -12,8 +12,15 @@
 void
 do_start(PGconn *conn)
 {
+	PGresult	*res;
+
 	/* call a function that start pg_statsinfo background process */
-	pgut_command(conn, "SELECT statsinfo.start(60)", 0, NULL);
+	res = PQexec(conn, "SELECT statsinfo.start(60)");
+
+	if (PQresultStatus(res) != PGRES_COMMAND_OK)
+		fprintf(stderr, "%s", PQerrorMessage(conn));
+
+	PQclear(res);
 }
 
 /*
@@ -22,6 +29,13 @@ do_start(PGconn *conn)
 void
 do_stop(PGconn *conn)
 {
+	PGresult	*res;
+
 	/* call a function that stop pg_statsinfo background process */
-	pgut_command(conn, "SELECT statsinfo.stop(60)", 0, NULL);
+	res = PQexec(conn, "SELECT statsinfo.stop(60)");
+
+	if (PQresultStatus(res) != PGRES_COMMAND_OK)
+		fprintf(stderr, "%s", PQerrorMessage(conn));
+
+	PQclear(res);
 }
